faec1f493394a1641d480c242ad9ef233888c1a2
Bruno Kemel <br.kemel@gmail.com>
2026-03-20 09:28:21 -0300

    testes

---
+++ teste/teste_raspagem_270.py
@@ -0,0 +1,10 @@
+# pular_ate = "ANA PAULA DA SILVA" ===> Passo 1
+# encontrado = False
+
+
+    # if not encontrado: ===> Passo 2
+    #     if nome == pular_ate:
+    #         encontrado = True
+    #     else:
+    #         print(f"Pulando: {nome}")
+    #         continue
