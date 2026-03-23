34cc30d00d7921cf2dccd5ea8380164d207f5681
Bruno Kemel <br.kemel@gmail.com>
2026-03-23 12:44:26 -0300

    import sys

--- raspagem_270.py
+++ raspagem_270.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
+import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from database.db import DBHandler, Ficha
